9217c1facbbf84842918c51fb15bf3f0c7c364a0
John Jennings <john@johncjennings.com>
2015-07-10 18:42:50 -0400

    Form checkbox stuff

---
+++ src/AppBundle/Resources/assets/js/toolkit.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+  $('input:checkbox').each( function() {
+    var id = $(this).attr('id');
+    $(this).addClass('form-checkbox').hide();
+    $('label[for=' + id + ']').addClass('checkbox-replace');
+    if ( $(this).is(':checked') ) {
+      $('label[for=' + id + ']').addClass('checked');
+    };
+  });
+
+  $('.form-checkbox').change(function() {
+    var id = $(this).attr('id');
+    $('label[for=' + id + ']').toggleClass('checked');
+  });
+
+})(jQuery);
