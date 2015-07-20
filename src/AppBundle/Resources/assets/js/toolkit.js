453f173d2e7f31a5f16673467a959cbf67d589d7
John Jennings <john@johncjennings.com>
2015-07-20 18:00:50 -0400

    MDL commit #4

--- src/AppBundle/Resources/assets/js/toolkit.js
+++ src/AppBundle/Resources/assets/js/toolkit.js
@@ -1,17 +1,29 @@
 (function ($) {
-  /*
-  $('input:checkbox').each( function() {
-    var id = $(this).attr('id');
-    $(this).addClass('form-checkbox').hide();
-    $('label[for=' + id + ']').addClass('checkbox-replace');
-    if ( $(this).is(':checked') ) {
-      $('label[for=' + id + ']').addClass('checked');
-    };
+
+  $('.mdl-layout__drawer .menu_level_1').each( function() {
+    var t = $(this);
+    t.parent().addClass('collapsed');
+    t.prev().addClass('menu-parent');
+    t.children('span').css( 'margin-top', '-' + $(this).css('height') );
+    t.css('overflow', 'hidden').hide();
   });
 
-  $('.form-checkbox').change(function() {
-    var id = $(this).attr('id');
-    $('label[for=' + id + ']').toggleClass('checked');
+  $('.menu-parent').click( function() { // $(this) is the link-like span
+    var t = $(this);
+    if ( t.parent().hasClass('collapsed') ) {
+      t.parent().removeClass('collapsed').addClass('expanded');
+      t.next().show();
+      t.next().children('span').animate({
+        marginTop: 0
+      }, 200 );
+    } else {
+      t.parent().removeClass('expanded').addClass('collapsed');
+      t.next().children('span').animate({
+        marginTop: '-' + $(this).next().css('height')
+      }, 200, 'swing', function() {
+        t.next().hide();
+      });
+    }
   });
-  */
+
 })(jQuery);
