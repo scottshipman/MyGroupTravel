aefc050c7eb0349324be288e111cf9594d1a7196
Scott Shipman <scott.shipman@tuidev.com>
2015-07-28 17:25:55 -0400

    one uploader drop zone and media Bundle - all the new files

---
+++ src/TUI/Toolkit/MediaBundle/Form/MediaType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace TUI\Toolkit\MediaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MediaType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('context', 'hidden')
+            ->add('filename', 'hidden')
+            ->add('hashedFilename', 'hidden', array(
+            'attr'=>array(
+              'class'=>'dropzone',
+            ),
+          ))
+          ;
+
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TUI\Toolkit\MediaBundle\Entity\Media'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'tui_toolkit_mediabundle_media';
+    }
+}
