021f5f52e7ba6809177cd077319bf13c881d568a
jordanvector <jvschin@gmail.com>
2015-08-24 16:32:00 -0400

    Change quote request form and controllers for emails

--- src/TUI/Toolkit/QuoteBundle/Controller/QuoteSiteController.php
+++ src/TUI/Toolkit/QuoteBundle/Controller/QuoteSiteController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use TUI\Toolkit\QuoteBundle\Entity\QuoteVersion;
-use TUI\Toolkit\QuoteBundle\Form\QuoteVersionType;
+use TUI\Toolkit\QuoteBundle\Form\QuoteChangeRequestType;
 use TUI\Toolkit\PermissionBundle\Entity\Permission;
 use TUI\Toolkit\PermissionBundle\Controller\PermissionService;
 use APY\DataGridBundle\Grid\Source\Entity;
@@ -15,15 +15,6 @@ use APY\DataGridBundle\Grid\Export\CSVExport;
 use APY\DataGridBundle\Grid\Action\RowAction;
 use Doctrine\ORM\Query\ResultSetMapping;
 
-use DeepCopy\DeepCopy;
-use DeepCopy\Filter\Doctrine\DoctrineCollectionFilter;
-use DeepCopy\Matcher\PropertyTypeMatcher;
-use DeepCopy\Filter\SetNullFilter;
-use DeepCopy\Matcher\PropertyNameMatcher;
-use DeepCopy\Filter\ReplaceFilter;
-use DeepCopy\Filter\KeepFilter;
-use DeepCopy\Matcher\PropertyMatcher;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * QuoteVersion controller.
@@ -32,67 +23,132 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 class QuoteSiteController extends Controller
 {
 
-  /**
-   * Finds and displays a QuoteVersion entity.
-   *
-   * param $id quoteReference id
-   *
-   */
-  public function siteShowAction($id, $quoteNumber = null)
-  {
-    $editable = false;
-    // TODO if user is allowed to edit then set $editable to true
-    // if organizer or if brand or higher (check permission table for organizer)
+    /**
+     * Finds and displays a QuoteVersion entity.
+     *
+     * param $id quoteReference id
+     *
+     */
+    public function siteShowAction($id, $quoteNumber = null)
+    {
+        $editable = false;
+        // TODO if user is allowed to edit then set $editable to true
+        // if organizer or if brand or higher (check permission table for organizer)
 
-    $em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager();
 
-    $locale = $this->container->getParameter('locale');
-    $date_format = $this->container->getParameter('date_format');
+        $locale = $this->container->getParameter('locale');
+        $date_format = $this->container->getParameter('date_format');
 
-    // Get all Quote versions referencing Parent Quote object
-    $entity = $em->getRepository('QuoteBundle:QuoteVersion')->findById($id);
-    if (!$entity) {
-      throw $this->createNotFoundException('Unable to find QuoteVersion entity.');
-    }
-
-
-    // get the content blocks to send to twig
-    $items=array();
-    $content = $entity[0]->getContent();
-    foreach($content as $tab){
-      foreach($tab as $key=>$block){
-        $object=$em->getRepository('ContentBlocksBundle:ContentBlock')->find($block);
-        if($object != null){
-          $items[$block] = $object;
+        // Get all Quote versions referencing Parent Quote object
+        $entity = $em->getRepository('QuoteBundle:QuoteVersion')->findById($id);
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find QuoteVersion entity.');
         }
-      }
+
+
+        // get the content blocks to send to twig
+        $items = array();
+        $content = $entity[0]->getContent();
+        foreach ($content as $tab) {
+            foreach ($tab as $key => $block) {
+                $object = $em->getRepository('ContentBlocksBundle:ContentBlock')->find($block);
+                if ($object != null) {
+                    $items[$block] = $object;
+                }
+            }
+        }
+
+        // get the content block that is the header block
+        $header = $entity[0]->getHeaderBlock();
+        if ($header != NULL) {
+            $headerBlock = $em->getRepository('ContentBlocksBundle:ContentBlock')->find($header);
+        } else {
+            $headerBlock = NULL;
+        }
+
+        // send warning messages
+        $warningMsg = array();
+        if ($entity[0]->getExpiryDate() < date($date_format)) {
+            $warningMsg[] = "This quote has expired. Please contact $entity>getQuoteReference()->getSalesAgent()->getFirstName()   $entity>getQuoteReference()->getSalesAgent()->getLasttName()  at $entity>getQuoteReference()->getSalesAgent()->getEmail()";
+        }
+
+
+        return $this->render('QuoteBundle:QuoteSite:siteShow.html.twig', array(
+            'entity' => $entity[0],
+            'locale' => $locale,
+            'items' => $items,
+            'warning' => $warningMsg,
+            'header' => $headerBlock,
+            'editable' => $editable,
+        ));
     }
 
-    // get the content block that is the header block
-    $header = $entity[0]->getHeaderBlock();
-    if($header !=NULL){
-      $headerBlock=$em->getRepository('ContentBlocksBundle:ContentBlock')->find($header);
-    } else {
-      $headerBlock = NULL;
+    /**
+     * Creates a form to make a change request to a QuoteVersion entity.
+     *
+     * @param QuoteVersion $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+
+    public function createChangeRequestForm($id)
+    {
+        $changeForm = $this->createForm(new QuoteChangeRequestType(), array(
+            'action' => $this->generateUrl('quote_site_change_request', array('id' => $id)),
+            'method' => 'POST',
+        ));
+
+        $changeForm->add('submit', 'submit', array('label' => 'Submit Change Request'));
+
+        return $changeForm;
+
     }
 
-    // send warning messages
-    $warningMsg = array();
-    if($entity[0]->getExpiryDate() < date($date_format)){
-      $warningMsg[] = "This quote has expired. Please contact $entity>getQuoteReference()->getSalesAgent()->getFirstName()   $entity>getQuoteReference()->getSalesAgent()->getLasttName()  at $entity>getQuoteReference()->getSalesAgent()->getEmail()";
+    public function requestChangeAction(Request $request, $id)
+    {
+
+        $changeForm = $this->createChangeRequestForm($id);
+        $changeForm->handleRequest($request);
+        $changes = $changeForm->get('changes')->getData();
+        $additional = $changeForm->get('additional')->getData();
+
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('QuoteBundle:QuoteVersion')->findById($id);
+        $entity = $entity[0];
+        $departure = $entity->getDepartureDate();
+        $tourName =  $entity->getName();
+        $salesAgent = $entity->getQuoteReference()->getSalesAgent();
+        $agentEmail = $salesAgent->getEmail();
+        $brand = $em->getRepository('BrandBundle:Brand')->findAll();
+        $brand = $brand[0];
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Quote Change Request')
+            ->setFrom('ChangeRequest@toolkit.com')
+            ->setTo($agentEmail)
+            ->setBody(
+                $this->renderView(
+                    'QuoteBundle:Emails:changerequest.html.twig',
+                    array(
+                        'brand' => $brand,
+                        'entity' => $entity,
+                        'changes' => $changes,
+                        'additional' => $additional,
+                        'departure' => $departure,
+                        'tour_name' => $tourName,
+                    )
+                ), 'text/html');
+
+        $this->get('mailer')->send($message);
+
+        return $this->render('QuoteBundle:QuoteSite:changeRequest.html.twig', array(
+            'change_request_form' => $changeForm->createView(),
+            'entity' => $entity,
+            'brand' => $brand,
+            'agent' => $salesAgent,
+        ));
     }
-
-
-
-    return $this->render('QuoteBundle:QuoteSite:siteShow.html.twig', array(
-      'entity'      => $entity[0],
-      'locale'      => $locale,
-      'items'       => $items,
-      'warning'     => $warningMsg,
-      'header'      => $headerBlock,
-      'editable'  =>  $editable,
-    ));
-  }
 
 
 }
