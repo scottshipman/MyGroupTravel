703a7a5a6fad2c91cab14428922983eda76976e3
Scott Shipman <scott.shipman@tuidev.com>
2015-08-25 08:55:04 -0400

    TOOL-56 Generate a share link prompt user for quote number process

--- src/TUI/Toolkit/QuoteBundle/Controller/QuoteSiteController.php
+++ src/TUI/Toolkit/QuoteBundle/Controller/QuoteSiteController.php
@@ -6,23 +6,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use TUI\Toolkit\QuoteBundle\Form\PromptType;
 use TUI\Toolkit\QuoteBundle\Entity\QuoteVersion;
-use TUI\Toolkit\QuoteBundle\Form\QuoteVersionType;
-use TUI\Toolkit\PermissionBundle\Entity\Permission;
-use TUI\Toolkit\PermissionBundle\Controller\PermissionService;
-use APY\DataGridBundle\Grid\Source\Entity;
-use APY\DataGridBundle\Grid\Export\CSVExport;
-use APY\DataGridBundle\Grid\Action\RowAction;
 use Doctrine\ORM\Query\ResultSetMapping;
 
-use DeepCopy\DeepCopy;
-use DeepCopy\Filter\Doctrine\DoctrineCollectionFilter;
-use DeepCopy\Matcher\PropertyTypeMatcher;
-use DeepCopy\Filter\SetNullFilter;
-use DeepCopy\Matcher\PropertyNameMatcher;
-use DeepCopy\Filter\ReplaceFilter;
-use DeepCopy\Filter\KeepFilter;
-use DeepCopy\Matcher\PropertyMatcher;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
@@ -40,6 +27,8 @@ class QuoteSiteController extends Controller
    */
   public function siteShowAction($id, $quoteNumber = null)
   {
+
+
     $editable = false;
     // TODO if user is allowed to edit then set $editable to true
     // if organizer or if brand or higher (check permission table for organizer)
@@ -53,6 +42,17 @@ class QuoteSiteController extends Controller
     $entity = $em->getRepository('QuoteBundle:QuoteVersion')->findById($id);
     if (!$entity) {
       throw $this->createNotFoundException('Unable to find QuoteVersion entity.');
+    }
+
+    // if no quoteNumber supplied in URL, then prompt for quoteNumber first
+    $securityContext = $this->get('security.context');
+    if($quoteNumber===NULL && FALSE === $securityContext->isGranted('ROLE_BRAND')){
+
+      $promptForm = $this->createPromptTypeForm($id);
+      return $this->render('QuoteBundle:QuoteSite:sitePrompt.html.twig', array(
+        'entity'      => $entity[0],
+        'form'        => $promptForm->createView(),
+      ));
     }
 
 
@@ -96,7 +96,55 @@ class QuoteSiteController extends Controller
   }
 
   /**
-   * Record Uhnique Views for a Quote View
+   * Create Quote Prompt Form
+   */
+
+  public function createPromptTypeForm($id)
+  {
+    $form = $this->createForm(new PromptType(), null, array(
+      'action' => $this->generateUrl('quote_site_validate', array('id' => $id)),
+      'method' => 'POST',
+    ));
+
+    $form->add('submit', 'submit', array('label' => 'Validate'));
+
+    return $form;
+  }
+
+  /**
+   * Validate a Quote Number matches the requested quote id
+   *
+   * redirect to full URL, else redirect to form page again
+   */
+
+  public function siteValidateAction(Request $request, $id)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $entity = $em->getRepository('QuoteBundle:QuoteVersion')->find($id);
+    if (!$entity) {
+      throw $this->createNotFoundException('Unable to find QuoteVersion entity.');
+    }
+    $promptForm = $this->createPromptTypeForm($id);
+    $promptForm->handleRequest($request);
+
+    $quoteNumber = $promptForm->get('quoteNumber')->getData();
+
+    $realQuoteNumber = $entity->getQuoteNumber();
+
+    if($quoteNumber==$realQuoteNumber){
+      // send to full page
+      return $this->redirect($this->generateUrl('quote_site_show', array('id' => $id, 'quoteNumber' => $realQuoteNumber)));
+    } else {
+    //send back to form page
+      $this->get('session')->getFlashBag()->add('notice', 'The Quote Number did not match our records for this requested Quote. Please try again or consult your Sales Contact.');
+      return $this->redirect($this->generateUrl('quote_site_action_show', array('id' => $id)));
+  }
+
+
+  }
+
+  /**
+   * Record Unique Views for a Quote View
    * @param $entity
    *
    * RULES:
@@ -113,7 +161,7 @@ class QuoteSiteController extends Controller
         if (strpos($_COOKIE['toolkit'], 'quote-' . $id . '~') !== FALSE) {
           return;
         }
-        $cookie = $_COOKIE['toolkit_quotes'] . '\n';
+        $cookie = $_COOKIE['toolkit'] . '\n';
       }
       $cookie .= 'quote-' . $id . '~';
       setcookie('toolkit', $cookie, time() + (365 * 24 * 60 * 60), "/"); // 1 year expiration
