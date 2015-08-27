a204c7e207d0bccdab9a77cc6f6d01700ef98ce2
John Jennings <john@johncjennings.com>
2015-08-27 12:13:03 -0400

    Footer text per Brand - initial code

--- src/TUI/Toolkit/BrandBundle/Form/BrandType.php
+++ src/TUI/Toolkit/BrandBundle/Form/BrandType.php
@@ -22,6 +22,17 @@ class BrandType extends AbstractType
             ->add('primaryColor')
             ->add('buttonColor')
             ->add('hoverColor')
+            ->add('footerBody', 'ckeditor', array(
+                'transformers' => array('html_purifier'),
+                'toolbar' => array('document', 'editing', 'tools', 'basicstyles'),
+                'toolbar_groups' => array(
+                    'document' => array('Source')
+                ),
+                'ui_color' => '#ffffff',
+                'startup_outline_blocks' => false,
+                'width' => '100%',
+                'height' => '320',
+            ))
             ->add('media', 'hidden', array(
                 'required' => false,
                 'data_class' => 'TUI\Toolkit\MediaBundle\Entity\Media',
