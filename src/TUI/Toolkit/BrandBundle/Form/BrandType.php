2108c0905619e07e14ea5b2d9236d2df39bb8bdb
John Jennings <john@johncjennings.com>
2015-09-02 10:37:00 -0400

    JS fix

--- src/TUI/Toolkit/BrandBundle/Form/BrandType.php
+++ src/TUI/Toolkit/BrandBundle/Form/BrandType.php
@@ -22,17 +22,7 @@ class BrandType extends AbstractType
             ->add('primaryColor')
             ->add('buttonColor')
             ->add('hoverColor')
-            ->add('footerBody', 'ckeditor', array(
-                'transformers' => array('html_purifier'),
-                'toolbar' => array('document', 'editing', 'tools', 'basicstyles'),
-                'toolbar_groups' => array(
-                    'document' => array('Source')
-                ),
-                'ui_color' => '#ffffff',
-                'startup_outline_blocks' => false,
-                'width' => '100%',
-                'height' => '320',
-            ))
+            ->add('footerBody', 'ckeditor', array())
             ->add('media', 'hidden', array(
                 'required' => false,
                 'data_class' => 'TUI\Toolkit\MediaBundle\Entity\Media',
