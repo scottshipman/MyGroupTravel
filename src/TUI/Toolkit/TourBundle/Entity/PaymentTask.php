c15739a191997b1999bf620fbea433e558324d4e
Scott Shipman <scott.shipman@tuidev.com>
2015-09-21 15:33:50 -0400

    payment task entity and crud, updates to tour form twig

---
+++ src/TUI/Toolkit/TourBundle/Entity/PaymentTask.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace TUI\Toolkit\TourBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PaymentTask
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class PaymentTask
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="value", type="float")
+     */
+    private $value;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dueDate", type="date")
+     */
+    private $dueDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="paidDate", type="date", nullable=true)
+     */
+    private $paidDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set string
+     *
+     * @param string $string
+     * @return PaymentTask
+     */
+    public function setString($string)
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+
+    /**
+     * Get string
+     *
+     * @return string 
+     */
+    public function getString()
+    {
+        return $this->string;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return PaymentTask
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set value
+     *
+     * @param float $value
+     * @return PaymentTask
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return float 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set dueDate
+     *
+     * @param \DateTime $dueDate
+     * @return PaymentTask
+     */
+    public function setDueDate($dueDate)
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    /**
+     * Get dueDate
+     *
+     * @return \DateTime 
+     */
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+
+    /**
+     * Set paidDate
+     *
+     * @param \DateTime $paidDate
+     * @return PaymentTask
+     */
+    public function setPaidDate($paidDate)
+    {
+        $this->paidDate = $paidDate;
+
+        return $this;
+    }
+
+    /**
+     * Get paidDate
+     *
+     * @return \DateTime 
+     */
+    public function getPaidDate()
+    {
+        return $this->paidDate;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return PaymentTask
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
---
+++ src/TUI/Toolkit/TourBundle/Form/PaymentTaskType.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace TUI\Toolkit\TourBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class PaymentTaskType extends AbstractType
+{
+
+  protected $paymentType;
+
+  public function __construct ($paymentType = null, $locale = null)
+  {
+    $this->paymentType = $paymentType;
+    $this->locale = $locale;
+  }
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+      switch ($this->locale) {
+        case 'en_GB.utf8':
+          $date_label = '(DD-MM-YYYY)';
+          $date_format = 'dd-MM-yyyy';
+          break;
+        default:
+          $date_label = '(MM-DD-YYYY)';
+          $date_format = 'MM-dd-yyyy';
+          break;
+      }
+
+        $builder
+            ->add('name')
+            ->add('value', 'text', array(
+              'label' =>  'Amount',
+            ))
+
+            ->add('dueDate', 'genemu_jquerydate', array(
+              'widget' => 'single_text',
+              'required' => true,
+              'label' => 'Due Date ' . $date_label,
+              'format' => $date_format,
+            ))
+
+
+            ->add('type', 'hidden', array( //make this hidden eventually
+                  'data' => $this->paymentType,
+            ))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TUI\Toolkit\TourBundle\Entity\PaymentTask',
+            'cascade_validation' => true,
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'tui_toolkit_tourbundle_paymenttask';
+    }
+}
