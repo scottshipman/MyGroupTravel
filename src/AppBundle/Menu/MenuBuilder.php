52921eaa13041678ecef6d4ac10080b862f70a0f
John Jennings <john@johncjennings.com>
2015-09-28 10:22:01 -0400

    Initial menu

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -25,31 +25,48 @@ class MenuBuilder
   {
     $menu = $this->factory->createItem('root');
 
-    $menu->addChild('Home', array('route' => '_homepage'));
-    $menu->addChild('Quotes', array('route' => 'manage_quote'));
-    $menu->addChild('Tours', array('route' => 'manage_tour'));
-    $menu->addChild('Users', array('route' => 'user'));
+    $menu->addChild('Home', array('route' => '_homepage'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
+    $menu->addChild('Quotes', array('route' => 'manage_quote'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
+    $menu->addChild('Tours', array('route' => 'manage_tour'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
+    $menu->addChild('Users', array('route' => 'user'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
 
     $menu->addChild('Reports')
-          ->setAttribute('dropdown', true);
-    $menu['Reports']->addChild('Modification Report', array('route' => '_tui_toolkit_reports_modification'));
-    $menu['Reports']->addChild('Opportunity Report', array('route' => '_tui_toolkit_reports_opportunity'));
+          ->setAttribute('dropdown', true)
+          ->setLabelAttribute('class', 'mdl-navigation__link');
+    $menu['Reports']->addChild('Modification Report', array('route' => '_tui_toolkit_reports_modification'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
+    $menu['Reports']->addChild('Opportunity Report', array('route' => '_tui_toolkit_reports_opportunity'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
 
 
     $menu->addChild('Company')
-          ->setAttribute('dropdown', true);
-    $menu['Company']->addChild('Brands', array('route' => '_manage_brand'));
-    $menu['Company']->addChild('Institutions', array('route' => 'manage_institution'));
+          ->setAttribute('dropdown', true)
+          ->setLabelAttribute('class', 'mdl-navigation__link');
+    $menu['Company']->addChild('Brands', array('route' => '_manage_brand'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
+    $menu['Company']->addChild('Institutions', array('route' => 'manage_institution'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
 
 
     $menu->addChild('Tools')
-      ->setAttribute('dropdown', true);
-    //$menu['Tools']->addChild('Content Blocks', array('route' => 'manage_contentblocks'));
-    $menu['Tools']->addChild('Currency', array('route' => 'manage_currency'));
-    $menu['Tools']->addChild('Board Basis', array('route' => '_manage_boardbasis'));
-    $menu['Tools']->addChild('Transport Type', array('route' => 'manage_transport'));
-    $menu['Tools']->addChild('Layout Types', array('route' => 'manage_layouttype'));
-    $menu['Tools']->addChild('Trip Status', array('route' => 'manage_tripstatus'));
+          ->setAttribute('dropdown', true)
+          ->setLabelAttribute('class', 'mdl-navigation__link');
+    /* $menu['Tools']->addChild('Content Blocks', array('route' => 'manage_contentblocks'))
+          ->setLinkAttribute('class', 'mdl-navigation__link'); */
+    $menu['Tools']->addChild('Currency', array('route' => 'manage_currency'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
+    $menu['Tools']->addChild('Board Basis', array('route' => '_manage_boardbasis'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
+    $menu['Tools']->addChild('Transport Type', array('route' => 'manage_transport'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
+    $menu['Tools']->addChild('Layout Types', array('route' => 'manage_layouttype'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
+    $menu['Tools']->addChild('Trip Status', array('route' => 'manage_tripstatus'))
+          ->setLinkAttribute('class', 'mdl-navigation__link');
 
     // ... add more children
 
