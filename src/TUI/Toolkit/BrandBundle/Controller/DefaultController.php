00c2f5ce73da5e7e3472dc7e2ba7d8e392e5e862
John Jennings <john@johncjennings.com>
2015-10-22 14:23:23 -0400

    TOOL-230 - Removed unused action

--- src/TUI/Toolkit/BrandBundle/Controller/DefaultController.php
+++ src/TUI/Toolkit/BrandBundle/Controller/DefaultController.php
@@ -42,23 +42,6 @@ class DefaultController extends Controller
       return $this->render('BrandBundle:Default:colors.html.php', array('brand' => $brand));
     }
 
-    public function colorsErrorAction()
-    {
-      $em = $this->getDoctrine()->getManager();
-      // Get the default/hardcoded Brand
-      $default_brand = $em->getRepository('BrandBundle:Brand')->findOneByName('ToolkitDefaultBrand');
-
-      // look for a configured brand
-      if($brand_id = $this->container->getParameter('brand_id')){
-        $brand = $em->getRepository('BrandBundle:Brand')->find($brand_id);
-      }
-
-      if(!$brand){
-        $brand = $default_brand;
-      }
-      return $this->render('BrandBundle:Default:colors-error.html.twig', array('brand' => $brand));
-    }
-
     public function footerAction()
     {
       $em = $this->getDoctrine()->getManager();
