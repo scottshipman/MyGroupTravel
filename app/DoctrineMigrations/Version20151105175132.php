2d5fb6f25f0443936276a761095c38180c177413
jordanvector <jvschin@gmail.com>
2015-11-06 10:06:45 -0500

    removed institution costraints from migration

--- app/DoctrineMigrations/Version20151105175132.php
+++ app/DoctrineMigrations/Version20151105175132.php
@@ -18,8 +18,6 @@ class Version20151105175132 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_3A9F98E55E237E06 ON institution (name)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_3A9F98E558E2FF25 ON institution (county)');
         $this->addSql('DROP INDEX unique_passenger ON passenger');
         $this->addSql('ALTER TABLE passenger ADD f_name VARCHAR(255) DEFAULT NULL, ADD l_name VARCHAR(255) DEFAULT NULL, DROP first_name, DROP last_name');
         $this->addSql('CREATE UNIQUE INDEX unique_passenger ON passenger (f_name, l_name, dob, tour)');
@@ -32,9 +30,7 @@ class Version20151105175132 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('DROP INDEX UNIQ_3A9F98E55E237E06 ON institution');
-        $this->addSql('DROP INDEX UNIQ_3A9F98E558E2FF25 ON institution');
+        
         $this->addSql('DROP INDEX unique_passenger ON passenger');
         $this->addSql('ALTER TABLE passenger ADD first_name VARCHAR(255) DEFAULT NULL, ADD last_name VARCHAR(255) DEFAULT NULL, DROP f_name, DROP l_name');
         $this->addSql('CREATE UNIQUE INDEX unique_passenger ON passenger (first_name, last_name, dob, tour)');
