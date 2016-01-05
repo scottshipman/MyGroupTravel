d4e30474c7e410bef58b0f87be4518d9e4fc5748
jordanvector <jvschin@gmail.com>
2016-01-05 13:44:26 -0500

    Medical entity CRUD

---
+++ src/TUI/Toolkit/PassengerBundle/Controller/MedicalController.php
@@ -0,0 +1,291 @@
+<?php
+
+namespace TUI\Toolkit\PassengerBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use TUI\Toolkit\PassengerBundle\Entity\Medical;
+use TUI\Toolkit\PassengerBundle\Form\MedicalType;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Medical controller.
+ *
+ */
+class MedicalController extends Controller
+{
+
+    /**
+     * Lists all Medical entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('PassengerBundle:Medical')->findAll();
+
+        return $this->render('PassengerBundle:Medical:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Medical entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Medical();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+        $reference = $form['passengerReference']->getData();
+
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $passenger = $em->getRepository('PassengerBundle:Passenger')->find($reference);
+            $entity->setPassengerReference($passenger);
+            $em->persist($entity);
+            $em->flush();
+
+            $passenger->setMedicalReference($entity);
+            $em->persist($passenger);
+            $em->flush();
+
+            $data = array(
+                $entity->getDoctorName(),
+                $entity->getDoctorNumber(),
+                $entity->getConditions(),
+                $entity->getMedications(),
+            );
+
+//            Decided to remove the ajax form from the create form
+//            $responseContent =  json_encode($data);
+//            return new Response($responseContent,
+//                Response::HTTP_OK,
+//                array('content-type' => 'application/json')
+//            );
+
+            return $this->redirect($this->generateUrl('manage_passenger_show', array('id' => $passenger->getId())));
+
+        }
+
+
+        return $this->redirect($this->generateUrl('manage_passenger_show', array('id' => $reference)));
+
+    }
+
+    /**
+     * Creates a form to create a Medical entity.
+     *
+     * @param Medical $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Medical $entity)
+    {
+        $locale = $this->container->getParameter('locale');
+        $form = $this->createForm(new MedicalType($locale), $entity, array(
+            'action' => $this->generateUrl('medical_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Medical entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Medical();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('PassengerBundle:Medical:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Medical entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PassengerBundle:Medical')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Medical entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('PassengerBundle:Medical:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Medical entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PassengerBundle:Medical')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Medical entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('PassengerBundle:Medical:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Medical entity.
+    *
+    * @param Medical $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Medical $entity)
+    {
+        $locale = $this->container->getParameter('locale');
+
+        $form = $this->createForm(new MedicalType($locale), $entity, array(
+            'action' => $this->generateUrl('medical_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+            'attr'  => array (
+                'id' => 'ajax_medical_form'
+            ),
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Medical entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PassengerBundle:Medical')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Medical entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        $passengerId = $entity->getPassengerReference();
+//        $passenger = $em->getRepository('PassengerBundle:Passenger')->find($passengerId);
+
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            $doctorNumber = $entity->getDoctorNumber();
+            $doctorNumberFormatted = $doctorNumber->getNationalNumber();
+
+            $data = array(
+                $entity->getDoctorName(),
+                $doctorNumberFormatted,
+                $entity->getConditions(),
+                $entity->getMedications(),
+            );
+
+            $responseContent =  json_encode($data);
+            return new Response($responseContent,
+                Response::HTTP_OK,
+                array('content-type' => 'application/json')
+            );
+
+
+//            return $this->redirect($this->generateUrl('manage_passenger_show', array('id' => $passenger->getId())));
+
+        }
+        $errors = $editForm->getErrors(true, true);
+
+        $errorCollection = array();
+        foreach($errors as $error){
+            $errorCollection[] = $error->getMessage();
+        }
+
+        $array = array( 'status' => 400, 'errorMsg' => 'Bad Request', 'errorReport' => $errorCollection);
+
+        $response = new Response( json_encode( $array ) );
+        $response->headers->set( 'Content-Type', 'application/json' );
+
+        return $response;// data to return via JSON
+//        return $this->redirect($this->generateUrl('manage_passenger_show', array('id' => $passenger->getId())));
+
+//        return $this->render('PassengerBundle:Medical:edit.html.twig', array(
+//            'entity'      => $entity,
+//            'edit_form'   => $editForm->createView(),
+//            'delete_form' => $deleteForm->createView(),
+//        ));
+    }
+    /**
+     * Deletes a Medical entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('PassengerBundle:Medical')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Medical entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('medical'));
+    }
+
+    /**
+     * Creates a form to delete a Medical entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('medical_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
---
+++ src/TUI/Toolkit/PassengerBundle/Entity/Medical.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace TUI\Toolkit\PassengerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Doctrine\Common\Annotations;
+use Gedmo\Mapping\Annotation as Gedmo;
+use APY\DataGridBundle\Grid\Mapping as GRID;
+use Doctrine\Common\Collections\ArrayCollection;
+/**
+ * Medical
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Medical
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="doctorName", type="text", nullable=true)
+     * @GRID\Column(title="Display Name", export=true)
+     */
+    private $doctorName;
+
+    /**
+     * @ORM\Column(type="phone_number", nullable=true)
+     */
+    private $doctorNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="conditions", type="text", nullable=true)
+     * @GRID\Column(title="Conditions", export=true)
+     */
+    private $conditions;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="medications", type="text", nullable=true)
+     * @GRID\Column(title="Medications", export=true)
+     */
+    private $medications;
+
+    /**
+     * @var \TUI\Toolkit\PassengerBundle\Entity\Passenger
+     * @ORM\OneToOne(targetEntity="TUI\Toolkit\PassengerBundle\Entity\Passenger")
+     * @ORM\JoinColumn(name="passenger", referencedColumnName="id")
+     */
+    protected $passengerReference;
+
+
+    /**
+     * @param $doctorName
+     * @return $this
+     *
+     */
+    public function setDoctorName($doctorName){
+
+        $this->doctorName = $doctorName;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     *
+     */
+
+    public function getDoctorName(){
+
+        return $this->doctorName;
+    }
+
+
+    /**
+     * @param $doctorNumber
+     * @return $this
+     *
+     */
+    public function setDoctorNumber($doctorNumber){
+
+        $this->doctorNumber = $doctorNumber;
+
+        return $this;
+    }
+
+    /**
+     * @return null
+     *
+     */
+
+    public function getDoctorNumber(){
+
+        return $this->doctorNumber;
+    }
+
+    /**
+     * @param $conditions
+     * @return $this
+     *
+     */
+    public function setConditions($conditions){
+
+        $this->conditions = $conditions;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     *
+     */
+
+    public function getConditions(){
+
+        return $this->conditions;
+    }
+
+    /**
+     * @param $conditions
+     * @return $this
+     *
+     */
+    public function setMedications($medications){
+
+        $this->medications = $medications;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     *
+     */
+
+    public function getMedications(){
+
+        return $this->medications;
+    }
+
+    /**
+     * @param $passengerReference
+     * @return $this
+     *
+     */
+    public function setPassengerReference($passengerReference) {
+
+        $this->passengerReference = $passengerReference;
+
+        return $this;
+
+    }
+
+    /**
+     * @return Medical
+     *
+     */
+    public function getPassengerReference() {
+        return $this->passengerReference;
+    }
+}
+
+
