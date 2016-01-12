ef061c1b2ac84ba348b97439aea583e24a39430c
scott shipman <k.scott.shipman@gmail.com>
2016-01-12 15:16:52 -0500

    status icons on passenger mini cards

--- src/TUI/Toolkit/PassengerBundle/Controller/PassengerController.php
+++ src/TUI/Toolkit/PassengerBundle/Controller/PassengerController.php
@@ -558,16 +558,19 @@ class PassengerController extends Controller
 
             foreach($passengers as $passenger) {
             $object = $passenger->getId();
+            $tourId = $passenger->getTourReference()->getId();
             $parent = $this->get("permission.set_permission")->getUser('parent', $object, 'passenger');
-            $isOrganizer = $this->get("permission.set_permission")->getUser('assistant', $object, 'tour') ? TRUE : FALSE;
-            $isOrganizer = $this->get("permission.set_permission")->getUser('organizer', $object, 'tour') ? TRUE : $isOrganizer;
 
             if (!empty($parent)){
                 $parentObject = $em->getRepository('TUIToolkitUserBundle:User')->find($parent[1]);
             } else {
                 $parentObject = "";
             }
-            $combinedObjects[]= array($passenger, $parentObject, $isOrganizer);
+                $isOrganizer = $this->get("permission.set_permission")->getPermission($tourId, 'tour', $parentObject)[0]=='organizer' ? TRUE : FALSE;
+                $isOrganizer = $this->get("permission.set_permission")->getPermission($tourId, 'tour', $parentObject)[0]=='assistant' ? TRUE : $isOrganizer;
+
+
+                $combinedObjects[]= array($passenger, $parentObject, $isOrganizer);
              }
 
             return $combinedObjects;
