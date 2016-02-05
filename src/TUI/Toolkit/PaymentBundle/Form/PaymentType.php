d578207ba53b6f73b86349e47ae147ea43d82b6a
scott shipman <k.scott.shipman@gmail.com>
2016-02-05 10:06:11 -0500

    tool-42 updating forms translations

--- src/TUI/Toolkit/PaymentBundle/Form/PaymentType.php
+++ src/TUI/Toolkit/PaymentBundle/Form/PaymentType.php
@@ -5,19 +5,53 @@ namespace TUI\Toolkit\PaymentBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
 
 class PaymentType extends AbstractType
 {
+    private $locale;
+    private $tour;
+    private $passenger;
+
+    public function __construct($locale, $tour, $passenger)
+    {
+        $this->passenger = $passenger;
+        $this->tour = $tour;
+    }
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+         switch (true) {
+            case strstr($this->locale, 'en_GB'):
+                $date_label = '(DD-MM-YYYY)';
+                $date_format = 'dd-MM-yyyy';
+                break;
+            default:
+                $date_label = '(MM-DD-YYYY)';
+                $date_format = 'MM-dd-yyyy';
+                break;
+        }
+
         $builder
-            ->add('value')
-            ->add('date')
-            ->add('note')
+            ->add('value' 'integer', array(
+                'label' => 'payment.form.value',
+                'translation_domain'  => 'messages',
+                'required' => true,
+            ))
+            ->add('date', 'genemu_jquerydate', array(
+                'widget' => 'single_text',
+                'required' => true,
+                'label' => 'payment.form.date',
+                'format' => $date_format,
+            ))
+            ->add('note', 'ckeditor', array(
+                'label' => 'payment.form.note',
+                'translation_domain'  => 'messages',
+            ));)
             ->add('tour')
             ->add('passenger')
         ;
@@ -29,7 +63,9 @@ class PaymentType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'TUI\Toolkit\PaymentBundle\Entity\Payment'
+            'data_class' => 'TUI\Toolkit\PaymentBundle\Entity\Payment',
+            'cascade_validation' => true,
+            'error_bubbling' => true,
         ));
     }
 
