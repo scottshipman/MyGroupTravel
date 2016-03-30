6ea4e9ba44e3900d35c01365a015a154eaf62f41
Colin Stillwell <itsme@colinstillwell.com>
2016-03-30 09:54:20 +0100

    TOOL-538 dropzone form close changed to use class for styles and js instead of id (ids will soon all be unique)

--- src/AppBundle/Resources/assets/js/toolkit_dropzone.js
+++ src/AppBundle/Resources/assets/js/toolkit_dropzone.js
@@ -4,13 +4,13 @@
     $(document).on('click', '.media-placeholder-image', function () {
         $('.media-placeholder-image').css({"display": "none"});
         $("#dropzone-form").css({"display": "block"});
-        $("#dropzone-form-close").css({"display": "inline-block"});
+        $(".dropzone-form-close").css({"display": "inline-block"});
     });
 
     $(document).on('click', '#avatar-label', function () {
         $('.media-placeholder-image').css({"display": "block"});
         $("#dropzone-form").css({"display": "none"});
-        $("#dropzone-form-close").css({"display": "none"});
+        $(".dropzone-form-close").css({"display": "none"});
     });
 
     $(document).on('submit', '#dropzone-form', function () {
