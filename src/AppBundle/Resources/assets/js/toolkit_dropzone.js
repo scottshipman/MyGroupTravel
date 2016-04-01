11caf77576a9cd1b2e7b8b4a7f492d5a6137e368
Colin Stillwell <itsme@colinstillwell.com>
2016-04-01 15:40:17 +0100

    TOOL-538 dropzone implementation modified to allow the disabling of default events

--- src/AppBundle/Resources/assets/js/toolkit_dropzone.js
+++ src/AppBundle/Resources/assets/js/toolkit_dropzone.js
@@ -4,42 +4,18 @@ Dropzone.autoDiscover = false;
 (function ($) {
   // Define a jquery function for dropzone.
   // This allows us to override default options.
-  $.fn.toolkitDropzone = function(media_field_id, options, existing_media) {
+  $.fn.toolkitDropzone = function(media_field_id, options, existing_media, disabled_events) {
     var dropzone_form = $(this);
     var dropzone_form_id = $(this).attr('id');
     var dropzone_form_close = $('.dropzone-form-close.' + dropzone_form_id);
     var media_field = $('#' + media_field_id);
     var media_placeholder_image = $('.media-placeholder-image.' + dropzone_form_id);
     var existing_media = existing_media || {};
-
+    var disabled_events = disabled_events || [];
     var default_options = {
       maxFiles: 1,
       acceptedMimeTypes: 'image/jpeg,image/png,image/jpg',
-      addRemoveLinks: true,
-      init: function () {
-        this.hiddenFileInput.removeAttribute('multiple');
-        this.on("success", function (file, response) {
-          mediaUpdate(
-            {
-              id: response.id,
-              path: response.relativepath,
-              filename: response.filename
-            }
-          );
-        });
-        this.on("addedfile", function () {
-          $(dropzone_form_close).css({"display": "none"});
-          if (this.files[1] != null) {
-            this.removeFile(this.files[0]);
-          }
-        });
-        this.on("removedfile", function (file) {
-          $(dropzone_form_close).css({"display": "block"});
-
-          // Revert media to existing.
-          mediaUpdate();
-        });
-      }
+      addRemoveLinks: true
     };
 
     var mediaUpdate = function(new_media) {
@@ -63,7 +39,43 @@ Dropzone.autoDiscover = false;
       }
     };
 
+    // Update media with existing.
+    mediaUpdate();
+
+    // Implement dropzone.
+    var dropzone = new Dropzone("#" + dropzone_form_id, jQuery.extend(default_options, options));
+
     // Dropzone manipulation.
+    if ($.inArray("success", disabled_events) == -1) {
+      dropzone.on("success", function (file, response) {
+        mediaUpdate(
+          {
+            id: response.id,
+            path: response.relativepath,
+            filename: response.filename
+          }
+        );
+      });
+    }
+
+    if ($.inArray("addedfile", disabled_events) == -1) {
+      dropzone.on("addedfile", function () {
+        $(dropzone_form_close).css({"display": "none"});
+        if (this.files[1] != null) {
+          this.removeFile(this.files[0]);
+        }
+      });
+    }
+
+    if ($.inArray("removedfile", disabled_events) == -1) {
+      dropzone.on("removedfile", function (file) {
+        $(dropzone_form_close).css({"display": "block"});
+
+        // Revert media to existing.
+        mediaUpdate();
+      });
+    }
+
     $(media_placeholder_image).click(function() {
       $(media_placeholder_image).css({"display": "none"});
       $(dropzone_form).css({"display": "block"});
@@ -77,11 +89,6 @@ Dropzone.autoDiscover = false;
       $(dropzone_form).css({"display": "none"});
       $(dropzone_form_close).css({"display": "none"});
     });
-
-    // Update media with existing.
-    mediaUpdate();
-
-    return this.dropzone($.extend(default_options, options));
   };
 
 })(jQuery);
