06def201f38086298ee762edf91a823bdeb77a69
Colin Stillwell <itsme@colinstillwell.com>
2016-04-27 12:13:28 +0100

    TOOL-626 context permissions added for the manage_headerblock_show route. additional property added for skipping permission checks when rendering from twig templates

--- src/TUI/Toolkit/ContentBlocksBundle/Controller/HeaderBlockController.php
+++ src/TUI/Toolkit/ContentBlocksBundle/Controller/HeaderBlockController.php
@@ -170,7 +170,7 @@ class HeaderBlockController extends Controller
      * Finds and displays a ContentBlock entity.
      *
      */
-    public function showAction($id, $quoteVersion = null, $class = null)
+    public function showAction($id, $quoteVersion = null, $class = null, $skipPermissions=false)
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -180,6 +180,11 @@ class HeaderBlockController extends Controller
 
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find Header Block entity.');
+        }
+
+        // Check context permissions.
+        if (!$skipPermissions) {
+            $this->get("permission.set_permission")->checkUserPermissions(TRUE, $class, $quoteVersion, ['organizer', 'assistant'], 'ROLE_BRAND');
         }
 
         return $this->render('ContentBlocksBundle:HeaderBlock:show.html.twig', array(
