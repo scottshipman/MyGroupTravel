555fc8282b4279b6eab48ab994000ed415a37534
Scott Pashley <github@scottpashley.co.uk>
2016-09-08 14:19:40 +0100

    TOOL-628 Updated migration to allow slug to be null (and be null by default)

--- app/DoctrineMigrations/Version20160901114643.php
+++ app/DoctrineMigrations/Version20160901114643.php
@@ -18,7 +18,7 @@ class Version20160901114643 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE tour ADD slug VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE tour ADD slug VARCHAR(255) DEFAULT NULL');
     }
 
     /**
