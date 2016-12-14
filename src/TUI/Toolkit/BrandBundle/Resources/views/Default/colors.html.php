1770f54c37a1ce48f87e11841f13e6b96f5f289b
Scott Pashley <github@scottpashley.co.uk>
2016-12-14 13:05:49 +0000

    TOOL-635 Only declare functions if they don't exist, otherwise errors cause output to render incorrectly

--- src/TUI/Toolkit/BrandBundle/Resources/views/Default/colors.html.php
+++ src/TUI/Toolkit/BrandBundle/Resources/views/Default/colors.html.php
@@ -1,271 +1,288 @@
 <?php
+if (!function_exists('rgb_hsl')) {
+    function rgb_hsl($rgb)
+    {
+        $r = $rgb[0];
+        $g = $rgb[1];
+        $b = $rgb[2];
 
-function rgb_hsl( $rgb ) {
-  $r = $rgb[0];
-  $g = $rgb[1];
-  $b = $rgb[2];
+        $r /= 255;
+        $g /= 255;
+        $b /= 255;
 
-	$r /= 255;
-	$g /= 255;
-	$b /= 255;
+        $max = max($r, $g, $b);
+        $min = min($r, $g, $b);
 
-  $max = max( $r, $g, $b );
-	$min = min( $r, $g, $b );
+        $h;
+        $s;
+        $l = ($max + $min) / 2;
+        $d = $max - $min;
 
-	$h;
-	$s;
-	$l = ( $max + $min ) / 2;
-	$d = $max - $min;
+        if ($d == 0) {
+            $h = $s = 0; // achromatic
+        } else {
+            $s = $d / (1 - abs(2 * $l - 1));
+            switch ($max) {
+                case $r:
+                    $h = 60 * fmod((($g - $b) / $d), 6);
+                    if ($b > $g) {
+                        $h += 360;
+                    }
+                    break;
+                case $g:
+                    $h = 60 * (($b - $r) / $d + 2);
+                    break;
+                case $b:
+                    $h = 60 * (($r - $g) / $d + 4);
+                    break;
+            }
+        };
 
-  if ( $d == 0 ) {
-    $h = $s = 0; // achromatic
-  } else {
-    $s = $d / ( 1 - abs( 2 * $l - 1 ) );
-		switch( $max ) {
-      case $r:
-        $h = 60 * fmod( ( ( $g - $b ) / $d ), 6 );
-        if ($b > $g) {
-          $h += 360;
+        return array(round($h, 3), round($s, 3), round($l, 3));
+    }
+}
+
+if (!function_exists('hsl_text')) {
+    function hsl_text($hsl, $l)
+    {
+        $h = $hsl[0];
+        return array($h, 1, $l);
+    }
+}
+
+if (!function_exists('hsl_background')) {
+    function hsl_background($hsl, $l)
+    {
+        $h = $hsl[0];
+        $s = $hsl[1];
+
+        return array($h, $s, $l);
+    }
+}
+
+if (!function_exists('hsl_rgb')) {
+    function hsl_rgb($hsl)
+    {
+        $h = $hsl[0];
+        $s = $hsl[1];
+        $l = $hsl[2];
+
+        $r;
+        $g;
+        $b;
+
+        $c = (1 - abs(2 * $l - 1)) * $s;
+        $x = $c * (1 - abs(fmod(($h / 60), 2) - 1));
+        $m = $l - ($c / 2);
+
+        if ($h < 60) {
+            $r = $c;
+            $g = $x;
+            $b = 0;
+        } else if ($h < 120) {
+            $r = $x;
+            $g = $c;
+            $b = 0;
+        } else if ($h < 180) {
+            $r = 0;
+            $g = $c;
+            $b = $x;
+        } else if ($h < 240) {
+            $r = 0;
+            $g = $x;
+            $b = $c;
+        } else if ($h < 300) {
+            $r = $x;
+            $g = 0;
+            $b = $c;
+        } else {
+            $r = $c;
+            $g = 0;
+            $b = $x;
+        };
+
+        $r = floor(($r + $m) * 255);
+        $g = floor(($g + $m) * 255);
+        $b = floor(($b + $m) * 255);
+
+        return 'rgb(' . $r . ', ' . $g . ', ' . $b . ')';
+    }
+}
+
+if (!function_exists('rgb_brightness')) {
+    function rgb_brightness($rgb)
+    {
+        $r = $rgb[0];
+        $g = $rgb[1];
+        $b = $rgb[2];
+
+        return (($r * 299) + ($g * 587) + ($b * 114)) / 1000; // W3C recommendation
+    }
+}
+
+if (!function_exists('color_contrast')) {
+    function color_contrast($colorArray)
+    {
+        if (rgb_brightness($colorArray) <= 125) {
+            return 'rgb(255,255,255)';
+        } else {
+            return 'rgb(66,66,66)';
         }
-        break;
-      case $g:
-        $h = 60 * ( ( $b - $r ) / $d + 2 );
-        break;
-      case $b:
-        $h = 60 * ( ( $r - $g ) / $d + 4 );
-        break;
-	  }
-	};
-
-	return array( round( $h, 3 ), round( $s, 3 ), round( $l, 3 ) );
-};
-
-function hsl_text( $hsl, $l ) {
-  $h = $hsl[0];
-
-  return array( $h, 1, $l );
-};
-
-function hsl_background( $hsl, $l ) {
-  $h = $hsl[0];
-  $s = $hsl[1];
-
-  return array( $h, $s, $l );
-};
-
-function hsl_rgb( $hsl ) {
-  $h = $hsl[0];
-  $s = $hsl[1];
-  $l = $hsl[2];
-
-  $r;
-  $g;
-  $b;
-
-	$c = ( 1 - abs( 2 * $l - 1 ) ) * $s;
-	$x = $c * ( 1 - abs( fmod( ( $h / 60 ), 2 ) - 1 ) );
-	$m = $l - ( $c / 2 );
-
-	if ( $h < 60 ) {
-		$r = $c;
-		$g = $x;
-		$b = 0;
-	} else if ( $h < 120 ) {
-		$r = $x;
-		$g = $c;
-		$b = 0;
-	} else if ( $h < 180 ) {
-		$r = 0;
-		$g = $c;
-		$b = $x;
-	} else if ( $h < 240 ) {
-		$r = 0;
-		$g = $x;
-		$b = $c;
-	} else if ( $h < 300 ) {
-		$r = $x;
-		$g = 0;
-		$b = $c;
-	} else {
-		$r = $c;
-		$g = 0;
-		$b = $x;
-	};
-
-	$r = floor( ( $r + $m ) * 255 );
-	$g = floor( ( $g + $m ) * 255 );
-	$b = floor( ( $b + $m  ) * 255 );
-
-  return 'rgb(' . $r . ', ' . $g . ', ' . $b . ')';
-};
-
-function rgb_brightness( $rgb ) {
-  $r = $rgb[0];
-  $g = $rgb[1];
-  $b = $rgb[2];
-
-  return ( ($r * 299) + ($g * 587) + ($b * 114) ) / 1000; // W3C recommendation
-};
-
-function color_contrast( $colorArray ) {
-  if ( rgb_brightness($colorArray) <= 125 ) {
-    return 'rgb(255,255,255)';
-  } else {
-    return 'rgb(66,66,66)';
-  }
-};
+    }
+}
 
 $primary = preg_replace('/\s+/', '', $brand->getPrimaryColor());
 $secondary = preg_replace('/\s+/', '', $brand->getSecondaryColor());
 $tertiary = preg_replace('/\s+/', '', $brand->getTertiaryColor());
 
-$primaryArray = explode(',', str_replace(array('rgb(', ')'), array('', ''), $primary) );
-$secondaryArray = explode(',', str_replace(array('rgb(', ')'), array('', ''), $secondary) );
-$tertiaryArray = explode(',', str_replace(array('rgb(', ')'), array('', ''), $tertiary) );
+$primaryArray = explode(',', str_replace(array('rgb(', ')'), array('', ''), $primary));
+$secondaryArray = explode(',', str_replace(array('rgb(', ')'), array('', ''), $secondary));
+$tertiaryArray = explode(',', str_replace(array('rgb(', ')'), array('', ''), $tertiary));
 
-?><style>
-/* MDL color overrides */
-a {
-  color: <?php echo $secondary ?>;
-}
+?>
+<style>
+    /* MDL color overrides */
+    a {
+        color: <?php echo $secondary ?>;
+    }
 
-.mdl-color-text--primary-contrast {
-  color: <?php echo color_contrast($primaryArray) ?> !important;
-}
+    .mdl-color-text--primary-contrast {
+        color: <?php echo color_contrast($primaryArray) ?> !important;
+    }
 
-.mdl-button.mdl-button--colored {
-  color: <?php echo $primary ?>;
-}
+    .mdl-button.mdl-button--colored {
+        color: <?php echo $primary ?>;
+    }
 
-.mdl-color--primary {
-  background: <?php echo $primary ?> !important;
-}
+    .mdl-color--primary {
+        background: <?php echo $primary ?> !important;
+    }
 
-.mdl-spinner__layer {
-  border-color: <?php echo $secondary ?> !important;
-}
+    .mdl-spinner__layer {
+        border-color: <?php echo $secondary ?> !important;
+    }
 
-.mdl-button--raised.mdl-button--colored,
-.mdl-button--raised.mdl-button--colored:hover,
-.mdl-button--raised.mdl-button--colored:active,
-.mdl-button--raised.mdl-button--colored:focus,
-.mdl-button--raised.mdl-button--colored:focus:not(:active),
-.mdl-button--raised.mdl-button--colored:visited {
-  background-color: <?php echo $secondary ?>;
-  color: <?php echo color_contrast($secondaryArray) ?>;
-}
+    .mdl-button--raised.mdl-button--colored,
+    .mdl-button--raised.mdl-button--colored:hover,
+    .mdl-button--raised.mdl-button--colored:active,
+    .mdl-button--raised.mdl-button--colored:focus,
+    .mdl-button--raised.mdl-button--colored:focus:not(:active),
+    .mdl-button--raised.mdl-button--colored:visited {
+        background-color: <?php echo $secondary ?>;
+        color: <?php echo color_contrast($secondaryArray) ?>;
+    }
 
-.brand_logo_login,
-.mdl-layout__header {
-  background-color: <?php echo $tertiary ?>;
-  color: <?php echo color_contrast($tertiaryArray) ?>;
-}
+    .brand_logo_login,
+    .mdl-layout__header {
+        background-color: <?php echo $tertiary ?>;
+        color: <?php echo color_contrast($tertiaryArray) ?>;
+    }
 
-.sub-header,
-.mdl-mini-footer {
-  background-color: <?php echo $primary ?>;
-  color: <?php echo color_contrast($primaryArray) ?>;
-}
+    .sub-header,
+    .mdl-mini-footer {
+        background-color: <?php echo $primary ?>;
+        color: <?php echo color_contrast($primaryArray) ?>;
+    }
 
-.mdl-layout__drawer .mdl-navigation .active a {
-  color: <?php echo hsl_rgb(hsl_text(rgb_hsl($secondaryArray), 0.25)) ?>;
-}
+    .mdl-layout__drawer .mdl-navigation .active a {
+        color: <?php echo hsl_rgb(hsl_text(rgb_hsl($secondaryArray), 0.25)) ?>;
+    }
 
-.mdl-textfield--floating-label.is-focused .mdl-textfield__label,
-.mdl-textfield--floating-label.is-dirty .mdl-textfield__label,
-.mdl-label-mimic {
-  color: <?php echo hsl_rgb(hsl_text(rgb_hsl($primaryArray), 0.25)) ?>;
-}
+    .mdl-textfield--floating-label.is-focused .mdl-textfield__label,
+    .mdl-textfield--floating-label.is-dirty .mdl-textfield__label,
+    .mdl-label-mimic {
+        color: <?php echo hsl_rgb(hsl_text(rgb_hsl($primaryArray), 0.25)) ?>;
+    }
 
-.mdl-textfield__label::after {
-  background-color: <?php echo $primary ?>;
-}
+    .mdl-textfield__label::after {
+        background-color: <?php echo $primary ?>;
+    }
 
-.mdl-checkbox.is-checked .mdl-checkbox__box-outline,
-.mdl-radio.is-checked .mdl-radio__outer-circle {
-  border-color: <?php echo $primary ?>;
-}
+    .mdl-checkbox.is-checked .mdl-checkbox__box-outline,
+    .mdl-radio.is-checked .mdl-radio__outer-circle {
+        border-color: <?php echo $primary ?>;
+    }
 
-.mdl-checkbox.is-checked .mdl-checkbox__tick-outline,
-.mdl-radio__inner-circle {
-  background-color: <?php echo $primary ?>;
-}
+    .mdl-checkbox.is-checked .mdl-checkbox__tick-outline,
+    .mdl-radio__inner-circle {
+        background-color: <?php echo $primary ?>;
+    }
 
-.mdl-tabs.is-upgraded .mdl-tabs__tab.is-active::after,
-.mdl-tabs-no-swap .is-active::after {
-  background-color: <?php echo $secondary ?>;
-}
+    .mdl-tabs.is-upgraded .mdl-tabs__tab.is-active::after,
+    .mdl-tabs-no-swap .is-active::after {
+        background-color: <?php echo $secondary ?>;
+    }
 
-.page-title .mdl-button--raised.mdl-button--colored,
-.page-title .mdl-button--raised.mdl-button--colored:hover {
-  background-color: <?php echo $secondary ?>;
-  color: <?php echo color_contrast($secondaryArray) ?>;
-}
+    .page-title .mdl-button--raised.mdl-button--colored,
+    .page-title .mdl-button--raised.mdl-button--colored:hover {
+        background-color: <?php echo $secondary ?>;
+        color: <?php echo color_contrast($secondaryArray) ?>;
+    }
 
-.mdl-progress>.progressbar {
-  background-color: <?php echo $secondary ?>;
-}
+    .mdl-progress > .progressbar {
+        background-color: <?php echo $secondary ?>;
+    }
 
-.mdl-progress>.bufferbar {
- background-image: linear-gradient(to right,rgba(255,255,255,.7),rgba(255,255,255,.7)),linear-gradient(to right,<?php echo $secondary ?>,<?php echo $secondary ?>);
-}
+    .mdl-progress > .bufferbar {
+        background-image: linear-gradient(to right, rgba(255, 255, 255, .7), rgba(255, 255, 255, .7)), linear-gradient(to right,<?php echo $secondary ?>,<?php echo $secondary ?>);
+    }
 
-/* Custom MDL matching colors */
-.tui-text-avatar {
-  background-color: <?php echo $primary ?>;
-  color: <?php echo color_contrast($primaryArray) ?>;
-}
+    /* Custom MDL matching colors */
+    .tui-text-avatar {
+        background-color: <?php echo $primary ?>;
+        color: <?php echo color_contrast($primaryArray) ?>;
+    }
 
-.content-block h2 {
-  color: <?php echo $primary ?>;
-}
+    .content-block h2 {
+        color: <?php echo $primary ?>;
+    }
 
-.add-content-block {
-  border-color: <?php echo hsl_rgb(hsl_text(rgb_hsl($secondaryArray), 0.25)) ?>;
-  color: <?php echo hsl_rgb(hsl_text(rgb_hsl($secondaryArray), 0.25)) ?>;
-}
+    .add-content-block {
+        border-color: <?php echo hsl_rgb(hsl_text(rgb_hsl($secondaryArray), 0.25)) ?>;
+        color: <?php echo hsl_rgb(hsl_text(rgb_hsl($secondaryArray), 0.25)) ?>;
+    }
 
-.site-content-block-actions {
-  background-color: <?php echo $primary ?>;
-  color: <?php echo color_contrast($primaryArray) ?>;
-}
+    .site-content-block-actions {
+        background-color: <?php echo $primary ?>;
+        color: <?php echo color_contrast($primaryArray) ?>;
+    }
 
-.mode-edit .inner-wrapper:hover {
-  outline-color: <?php echo $primary ?>;
-}
+    .mode-edit .inner-wrapper:hover {
+        outline-color: <?php echo $primary ?>;
+    }
 
-.sp-replacer.sp-active,
-.sp-replacer:hover,
-.sp-container {
-  border-color: <?php echo $primary ?>;
-}
+    .sp-replacer.sp-active,
+    .sp-replacer:hover,
+    .sp-container {
+        border-color: <?php echo $primary ?>;
+    }
 
-.media-placeholder-image,
-.dropzone-form {
-  border-color: <?php echo $primary ?>;
-}
+    .media-placeholder-image,
+    .dropzone-form {
+        border-color: <?php echo $primary ?>;
+    }
 
-#avatar-label {
-  color: <?php echo hsl_rgb(hsl_text(rgb_hsl($primaryArray), 0.25)) ?>;
-}
+    #avatar-label {
+        color: <?php echo hsl_rgb(hsl_text(rgb_hsl($primaryArray), 0.25)) ?>;
+    }
 
-.existing-media-wrapper .existing-media-item .existing-delete {
-  color: <?php echo $primary ?>;
-}
+    .existing-media-wrapper .existing-media-item .existing-delete {
+        color: <?php echo $primary ?>;
+    }
 
-.tab-reorder-modal .submit-button-container .reorder-button {
-  color: <?php echo $primary ?>;
-}
+    .tab-reorder-modal .submit-button-container .reorder-button {
+        color: <?php echo $primary ?>;
+    }
 
-.alt-quote.even {
-  color: <?php echo hsl_rgb(hsl_text(rgb_hsl($secondaryArray), 0.25)) ?>;
-}
+    .alt-quote.even {
+        color: <?php echo hsl_rgb(hsl_text(rgb_hsl($secondaryArray), 0.25)) ?>;
+    }
 
-.alt-quote.even:hover {
-  color: <?php echo hsl_rgb(hsl_text(rgb_hsl($secondaryArray), 0.125)) ?>;
-}
+    .alt-quote.even:hover {
+        color: <?php echo hsl_rgb(hsl_text(rgb_hsl($secondaryArray), 0.125)) ?>;
+    }
 
-.profile-tour-header:before {
-  background: linear-gradient(to bottom right, <?php echo hsl_rgb(hsl_background(rgb_hsl($tertiaryArray), 0.3)) ?> 0%, <?php echo hsl_rgb(hsl_background(rgb_hsl($tertiaryArray), 0.7)) ?> 100%);
-}
+    .profile-tour-header:before {
+        background: linear-gradient(to bottom right, <?php echo hsl_rgb(hsl_background(rgb_hsl($tertiaryArray), 0.3)) ?> 0%, <?php echo hsl_rgb(hsl_background(rgb_hsl($tertiaryArray), 0.7)) ?> 100%);
+    }
 </style>
